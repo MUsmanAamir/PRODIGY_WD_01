017d045fd980c8e3c95858e8c05d0fa94ebab50a
usman <usmanaamirbs2022@gmail.com>
2024-06-23 15:02:24 +0500

    light/dark theme added

---
+++ html landing page/app.js
@@ -0,0 +1,27 @@
+var toggle_btn = document.querySelector(".toggle-btn");
+var big_wrapper = document.querySelector(".big-wrapper");
+const main = document.querySelector("main");
+
+let dark = false;
+
+function toggleAnimation()
+{
+    //clone the big wrapper
+    dark=!dark;
+    console.log(dark);
+    let clone = big_wrapper.cloneNode(true);
+    if(dark)
+    {
+        clone.classList.remove("light");
+        clone.classList.add("dark");
+    }
+    else
+    {
+        clone.classList.remove("dark");
+        clone.classList.add("light");
+    }
+    clone.classList.add("copy");
+    main.appendChild(clone);
+}
+
+toggle_btn.addEventListener("click",toggleAnimation);
